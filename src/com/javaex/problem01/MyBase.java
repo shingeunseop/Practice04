97f4441ac8cf2675a5b65e73393c4dfb2ee72619
remys <remys@remys-notebook>
2017-07-10 14:35:37 +0900

    연습문제04

---
+++ src/com/javaex/problem01/MyBase.java
@@ -0,0 +1,7 @@
+package com.javaex.problem01;
+
+public class MyBase extends Base{
+
+    //코드작성
+    
+}
---
+++ src/com/javaex/problem02/Depart.java
@@ -0,0 +1,7 @@
+package com.javaex.problem02;
+
+public class Depart extends Employee {
+
+    //코드작성
+
+}
