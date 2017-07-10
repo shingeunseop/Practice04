0c712ed650e04d94f16c4e1966ed03f3a6813101
BIT <BIT@DESKTOP-DOGVD1T>
2017-07-10 17:55:30 +0900

    7/10 연습문제 4 1번풀이완료

--- src/com/javaex/problem01/MyBase.java
+++ src/com/javaex/problem01/MyBase.java
@@ -1,7 +1,23 @@
 package com.javaex.problem01;
 
 public class MyBase extends Base{
+	
+	public void day() {
+		System.out.println("낮에는 열심히 일하자");
+	}
 
+    public void service(String state) {
+
+        if (state.equals("낮")) {
+            day();
+        } else if(state.equals("오후")){
+        	System.out.println("오후도 낮과 마찬가지로 일해야 합니다.");}
+        else {
+        		night();}
+        
+    }
+	
     //코드작성
     
+	
 }
