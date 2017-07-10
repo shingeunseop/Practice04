4185c4078ab55c82a22bd95ba7037ebe9b388ad9
BIT <BIT@DESKTOP-DOGVD1T>
2017-07-10 18:03:23 +0900

    7/10 연습문제 4 2번문제

--- src/com/javaex/problem02/Depart.java
+++ src/com/javaex/problem02/Depart.java
@@ -1,6 +1,26 @@
 package com.javaex.problem02;
 
 public class Depart extends Employee {
+	private String department;
+
+	public Depart(String name, int salary, String department) {
+		super(name,salary);
+		this.department = department;
+		
+	}
+	public void getInformation() {
+		System.out.println("이름:" + super.getName() + " 연봉:" + super.getSalary()+" 부서:"+department);
+		
+	}
+
+	public String getDepartment() {
+		return department;
+	}
+
+	public void setDepartment(String department) {
+		this.department = department;
+	}
+	
 
     //코드작성
 
