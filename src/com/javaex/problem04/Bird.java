38c9bb962ed4b19a3e90dab10be57a55912677bc
myhome <myhome@175.195.155.96>
2017-07-11 00:26:48 +0900

    7/10 연습문제 4 4번문제

--- src/com/javaex/problem04/Bird.java
+++ src/com/javaex/problem04/Bird.java
@@ -2,6 +2,24 @@ package com.javaex.problem04;
 
 public abstract class Bird {
     private String name;
+    
+    abstract void fly();
+    abstract void sing();
+    abstract void showName();
+
+	public Bird(String name) {
+		super();
+		this.name = name;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+    
 
 
 
