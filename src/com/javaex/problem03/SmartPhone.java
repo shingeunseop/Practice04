a97444dc4c12548f290eb7eff43fd5497d1daef2
myhome <myhome@175.195.155.96>
2017-07-10 23:41:58 +0900

    7/10 연습문제4 3번문제 풀이 완료

--- src/com/javaex/problem03/SmartPhone.java
+++ src/com/javaex/problem03/SmartPhone.java
@@ -3,9 +3,24 @@ package com.javaex.problem03;
 public class SmartPhone extends MusicPhone {
     
     public void execute(String str){
+    	 if("음악".equals(str)){
+             playMusic();
+         }else if("앱".equals(str)){
+             playApp();
+         }else
+        	 call();
         
         //코드작성
         
+    }
+    public void playMusic(){
+        System.out.println("다운로드해서 음악재생");
+    }
+    public void playApp() {
+    	System.out.println("앱실행");
+    }
+    public void call() {
+    	System.out.println("통화기능 시작");
     }
  
     //메소드작성
